1310c074c7f7bce591bfc17734ae0c56834d49ff
juliocachaydev <julio.cachay.dev@gmail.com>
2025-07-24 12:45:47 -0400

    Added Interval value object

---
+++ Jcg.Domain.Core.Tests/ValueObjects/IntervalTests.cs
@@ -0,0 +1,113 @@
+﻿using System.ComponentModel.Design.Serialization;
+using Jcg.Domain.Core.Exceptions;
+using Jcg.Domain.Core.ValueObjects;
+
+namespace Jcg.Domain.Core.Tests.ValueObjects;
+
+public class IntervalTests
+{
+    [Theory]
+    [InlineData(10,15, false)]
+    [InlineData(10,10, false)]
+    [InlineData(10,9, true)]
+    public void Constructor_AssertsEndIsSameOrMoreThanStart(
+        int start, int end, bool shouldThrow)
+    {
+        // ***** ARRANGE *****
+
+        // ***** ACT *****
+        
+        var result = Record.Exception(() => new Interval<int>(start, end));
+
+        // ***** ASSERT *****
+
+        if (shouldThrow)
+        {
+            Assert.IsType<InvalidEntityStateException>(result);
+        }
+        else
+        {
+            Assert.Null(result);
+        }
+    }
+
+    [Theory]
+    // Only the ends of the interval overlap, but this is inclusive so that is an overlap
+    [InlineData(10,20,20,30,true)]
+    [InlineData(10,20,0,10, true)]
+    // One interval contained in another
+    [InlineData(0,10,1,9,true)]
+    // Both intervals are the same
+    [InlineData(0,10,0,10,true)]
+    // Some overlap
+    [InlineData(0,10,9,20, true)]
+    [InlineData(0,10,-10,1, true)]
+    // no overlap
+    [InlineData(0,10,11,20,false)]
+    [InlineData(0,10,-10,-1, false)]
+    public void OverlapsInclusiveWith_ChecksIfOverlapsWithAnotherInterval_IncludingStartAndEnd(
+        int s1, int e1, int s2, int e2, bool shouldOverlap)
+    {
+        // ***** ARRANGE *****
+
+        var i1 = new Interval<int>(s1, e1);
+        var i2 = new Interval<int>(s2, e2);
+
+        // ***** ACT *****
+
+        var result = i1.OverlapsInclusiveWith(i2);
+
+        // ***** ASSERT *****
+        
+        Assert.Equal(shouldOverlap, result);
+    }
+
+    [Theory]
+    // Only the ends of the interval overlap, but this is exclusive so that is not an overlap
+    [InlineData(10,20,20,30,false)]
+    [InlineData(10,20,0,10, false)]
+    // One interval contained in another
+    [InlineData(0,10,1,9,true)]
+    // Both intervals are the same
+    [InlineData(0,10,0,10,true)]
+    // Some overlap
+    [InlineData(0,10,9,20, true)]
+    [InlineData(0,10,-10,1, true)]
+    // no overlap
+    [InlineData(0,10,11,20,false)]
+    [InlineData(0,10,-10,-1, false)]
+    public void OverlapsExclusiveWith_ChecksIfOverlapsWithAnotherInterval_ExcludingStartAndEnd(
+        int s1, int e1, int s2, int e2, bool shouldOverlap)
+    {
+        // ***** ARRANGE *****
+
+        var i1 = new Interval<int>(s1, e1);
+        var i2 = new Interval<int>(s2, e2);
+
+        // ***** ACT *****
+
+        var result = i1.OverlapsExclusiveWith(i2);
+
+        // ***** ASSERT *****
+        
+        Assert.Equal(shouldOverlap, result);
+    }
+
+    [Fact]
+    public void CanMapIntervalToDifferenType()
+    {
+        // ***** ARRANGE *****
+        
+        var i1 = new Interval<int>(100, 200);
+
+        // ***** ACT *****
+
+        var i2 = i1.MapTo(x => x.ToString());
+
+        // ***** ASSERT *****
+
+        Assert.IsType<Interval<String>>(i2);
+        Assert.Equal("100", i2.Start);
+        Assert.Equal("200", i2.End);
+    }
+}
---
+++ Jcg.Domain.Core/ValueObjects/Interval.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Jcg.Domain.Core.Exceptions;
+
+namespace Jcg.Domain.Core.ValueObjects;
+
+/// <summary>
+/// Models an interval with a start and end value.
+/// </summary>
+public record Interval<T> where T: IComparable<T>
+{
+    /// <summary>
+    /// The Start Value of the Interval.
+    /// </summary>
+    public T Start { get; }
+
+    /// <summary>
+    /// The End value of the interval. Is always greater than or equal to Start.
+    /// </summary>
+    public T End { get; }
+    
+    /// <summary>
+    /// Creates an Interval, asserting that the End value of the interval must be greater than or equal to the Start value.
+    /// </summary>
+    /// <param name="start">The start value of the interval.</param>
+    /// <param name="end">The end value of the interval.</param>
+    /// <exception cref="InvalidEntityStateException">
+    /// Thrown when the end value is less than the start value.
+    /// </exception>
+    public Interval(T start, T end)
+    {
+        if (end.CompareTo(start) < 0)
+        {
+            throw new InvalidEntityStateException("End must be greater than or equal to start.");
+        }
+
+        Start = start;
+        End = end;
+    }
+
+    /// <summary>
+    /// Determines whether this interval overlaps inclusively with another interval.
+    /// Inclusive overlap means the intervals share at least one value, including endpoints.
+    /// </summary>
+    /// <param name="other">The other interval to check for overlap.</param>
+    /// <returns>
+    /// True if the intervals overlap inclusively; otherwise, false.
+    /// </returns>
+    public bool OverlapsInclusiveWith(Interval<T> other)
+    {
+        // Overlap occurs if the start of one interval is less than or equal to the end of the other, and vice versa
+        return Start.CompareTo(other.End) <= 0 && End.CompareTo(other.Start) >= 0;
+    }
+    
+    /// <summary>
+    /// Determines whether this interval overlaps exclusively with another interval.
+    /// Exclusive overlap means the intervals share at least one value, but not at their endpoints.
+    /// </summary>
+    /// <param name="other">The other interval to check for exclusive overlap.</param>
+    /// <returns>
+    /// True if the intervals overlap exclusively; otherwise, false.
+    /// </returns>
+    public bool OverlapsExclusiveWith(Interval<T> other)
+    {
+        // Exclusive overlap: intervals share values, but not at endpoints
+        return Start.CompareTo(other.End) < 0 && End.CompareTo(other.Start) > 0;
+    }
+    
+    /// <summary>
+    /// Maps the start and end values of this interval to a new type using the provided mapping function.
+    /// </summary>
+    /// <typeparam name="U">The type to map the interval values to. Must implement <see cref="IComparable{U}"/>.</typeparam>
+    /// <param name="mapFunc">A function that maps a value of type <typeparamref name="T"/> to type <typeparamref name="U"/>.</param>
+    /// <returns>
+    /// A new <see cref="Interval{U}"/> with the mapped start and end values.
+    /// </returns>
+    public Interval<U> MapTo<U>(Func<T, U> mapFunc) where U : IComparable<U>
+    {
+        var newStart = mapFunc(Start);
+        var newEnd = mapFunc(End);
+        return new Interval<U>(newStart, newEnd);
+    }
+    
+    
+}
